2f28962d4a69a4a687e10a4eb0e85678da6db7f8
Andrew Dymond <andy.dymond@gmail.com>
2018-04-04 19:00:34 -0600

    Migrate user

---
+++ db/migrate/20180404200407_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :token
+      t.string :refresh_token
+      t.datetime :oauth_expires_at
+      t.text :image_url
+
+      t.timestamps
+    end
+  end
+end
