503c9466081db1b6966bb50de86a749def82503f
Andrew Dymond <andy.dymond@gmail.com>
2018-04-05 15:08:26 -0600

    User can create account w/o google account

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
 
-  resources :dashboard, only: :index
+  resources :dashboard, only: [:index]
+
+  resources :users, only: [:create]
 
   get "/auth/google", as: :google_login
   get 'auth/:provider/callback',  to: 'sessions#create'
