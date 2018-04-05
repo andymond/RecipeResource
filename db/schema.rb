f8bd6bd2ab7fcf62b41b961bf1fdf66bb3ca89e0
Andrew Dymond <andy.dymond@gmail.com>
2018-04-05 16:07:48 -0600

    Add google credentials table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405212303) do
+ActiveRecord::Schema.define(version: 20180405212805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20180405212303) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_google_credentials_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema.define(version: 20180405212303) do
   end
 
   add_foreign_key "app_credentials", "users"
+  add_foreign_key "google_credentials", "users"
 end
