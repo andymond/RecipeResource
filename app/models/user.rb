cd4f095901bce36f09ab61bff19d3a5c29cb189d
Andrew Dymond <andy.dymond@gmail.com>
2018-04-05 16:08:51 -0600

    Add google_credentials relationship to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,27 +3,33 @@ class User < ApplicationRecord
   validates_presence_of :email
   before_save :authenticate
   has_one :app_credential, dependent: :destroy
+  has_one :google_credential, dependent: :destroy
 
   def self.update_or_create(auth)
-   user = User.find_by(uid: auth[:uid]) || User.new
+   gc = GoogleCredential.find_by(uid: auth[:uid]) || GoogleCredential.new
+   gc.attributes = {
+         provider: auth[:provider],
+         uid: auth[:uid],
+         token: auth[:credentials][:token],
+         refresh_token: auth[:credentials][:refresh_token],
+         oauth_expires_at: auth[:credentials][:expires_at]
+       }
+   user = gc.create_user
    user.attributes = {
-     provider: auth[:provider],
-     uid: auth[:uid],
-     email: auth[:info][:email],
-     first_name: auth[:info][:first_name],
-     last_name: auth[:info][:last_name],
-     image_url: auth[:info][:image],
-     token: auth[:credentials][:token],
-     refresh_token: auth[:credentials][:refresh_token],
-     oauth_expires_at: auth[:credentials][:expires_at]
-   }
+         email: auth[:info][:email],
+         first_name: auth[:info][:first_name],
+         last_name: auth[:info][:last_name],
+         image_url: auth[:info][:image]
+       }
    user.save!
+   gc.save!
    user
   end
 
   private
 
     def authenticate
-      raise ActiveRecord::Rollback if app_credential.nil? || app_credential.password_digest.nil?
+      raise ActiveRecord::Rollback if (app_credential.nil? || app_credential.password_digest.nil?) && (google_credential.nil? || google_credential.uid.nil?)
     end
+
 end
