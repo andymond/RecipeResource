5f3476273523d81b26d09720fdf0776791affda8
Andrew Dymond <andy.dymond@gmail.com>
2018-04-05 16:09:13 -0600

    Reroute oauth login to gsessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.update_or_create(request.env["omniauth.auth"])
-    session[:user_id] = user.id
-    flash[:notice] = "Successfully registered with Google"
-    redirect_to dashboard_index_path
+
   end
 
   def destroy
