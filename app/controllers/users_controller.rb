e63e564b273381eeda2be506839d623a44725417
Andrew Dymond <andy.dymond@gmail.com>
2018-04-05 17:46:43 -0600

    Modify create action in users_controller to properly confirm user password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 
   def create
-    user = User.new(user_params)
-    user.create_app_credential(password: params[:password])
-    if user.save
+    app_credential = AppCredential.new(credential_params)
+    user = app_credential.create_user(user_params)
+    if app_credential.save && user.save
       flash[:notice] = "Account created!"
       session[:user_id] = user.id
       redirect_to dashboard_index_path
@@ -17,6 +17,10 @@ class UsersController < ApplicationController
 
     def user_params
       params.permit(:first_name, :last_name, :email)
+    end
+
+    def credential_params
+      params.permit(:password, :password_confirmation)
     end
 
 end
