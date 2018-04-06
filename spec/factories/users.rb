bce3898fc2deec4ff5a43c4495852760e4fd65a0
Andrew Dymond <andy.dymond@gmail.com>
2018-04-05 18:18:04 -0600

    Remove authenticate method from user model and add custom validator for user credentials

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,11 +3,9 @@ FactoryBot.define do
     sequence :first_name { |n| "first_name#{n}"}
     sequence :last_name { |n| "last_name#{n}"}
     sequence :email { |n| "email#{n}@email.com"}
-  end
-
-  factory :app_credential do
-    password "password"
-    user
+    before(:create) do |user|
+      user.create_app_credential(password: "password")
+    end
   end
 
 end
