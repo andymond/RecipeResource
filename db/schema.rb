038a7e136273140f0328963273a9bfdf6a675893
Andrew Dymond <andy.dymond@gmail.com>
2018-04-06 18:44:28 -0600

    Add roles, user_roles, zip to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180406231902) do
+ActiveRecord::Schema.define(version: 20180407002103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,20 @@ ActiveRecord::Schema.define(version: 20180406231902) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "zipcode"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "user_roles", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.bigint "restaurant_id"
+    t.index ["restaurant_id"], name: "index_user_roles_on_restaurant_id"
+    t.index ["role_id"], name: "index_user_roles_on_role_id"
+    t.index ["user_id"], name: "index_user_roles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,4 +70,7 @@ ActiveRecord::Schema.define(version: 20180406231902) do
 
   add_foreign_key "app_credentials", "users"
   add_foreign_key "google_credentials", "users"
+  add_foreign_key "user_roles", "restaurants"
+  add_foreign_key "user_roles", "roles"
+  add_foreign_key "user_roles", "users"
 end
