be4fcd23f48214eb6305728927c32e4571278fe3
Andrew Dymond <andy.dymond@gmail.com>
2018-04-07 14:01:27 -0600

    Validate uniqueness of restaurant name scoped to zipcode

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,5 @@ class Restaurant < ApplicationRecord
   has_many :user_roles
   has_many :roles, through: :user_roles
   has_many :users, through: :user_roles
+  validates_uniqueness_of :name, scope: [:zipcode]
 end
