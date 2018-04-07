b7e131c9106fc073864395239fcd71dbe7a81f90
Andrew Dymond <andy.dymond@gmail.com>
2018-04-07 13:57:29 -0600

    Add restaurant_spec to test relationships & validations

---
+++ spec/models/restaurant_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+describe Restaurant do
+  it {should have_many(:user_roles)}
+  it {should have_many(:roles)}
+  it {should have_many(:users)}
+  it {should validate_uniqueness_of(:name).scoped_to(:zipcode)}
+end
