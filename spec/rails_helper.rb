3826d0fb6511306ce4d635602cac62e5ba27100f
Andrew Dymond <andy.dymond@gmail.com>
2018-04-06 18:13:46 -0600

    Add user_can_register_with_restaurant_spec & yelp_response json

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,14 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'webmock/rspec'
+
+def stub_yelp_search(filename, url)
+  json_response = File.read("./spec/fixtures/#{filename}")
+  stub_request(:get, url)
+    .to_return(status: 200, body: "#{json_response}", headers: {Authorize: "Bearer #{ENV['YELP_API_KEY']}"})
+end
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -56,7 +64,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  
+
   config.before(:all) do
     DatabaseCleaner.clean
   end
