52419b1e3ce8176cbce76a799bc36659a929f587
Andrew Dymond <andy.dymond@gmail.com>
2018-04-07 20:01:47 -0600

    Add yelp_search_service & yelp_url to restaurant, add yelp updates to #set_restaurant in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ApplicationRecord
     set_default_role
     restaurant = Restaurant.create(name: attrs[:name], zipcode: attrs[:zipcode])
     user_roles.last.update(restaurant_id: restaurant.id)
+    search = YelpSearchService.new
+    search.update_restaurant(restaurant)
   end
 
   def self.update_or_create(auth)
