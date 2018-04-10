3e04ffef0476284ab4b73c445c92e42bf7058ab0
Andrew Dymond <andy.dymond@gmail.com>
2018-04-10 16:39:17 -0600

    Add user_can_register_with_restaurant_spec

---
+++ spec/features/chef_can_update_restaurant_info_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe "As a user with role 'chef'" do
+  it "allows me to edit restaurants' info" do
+    user = create(:chef)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    VCR.use_cassette "No Reviews" do
+      visit root_path
+      expect(current_path).to eq(dashboard_index_path)
+    end
+
+    VCR.use_cassette "No Reviews" do
+      click_on "Update Restaurant Info"
+      expect(current_path).to eq(edit_chef_restaurant_path(user.restaurants.first.slug))
+    end
+
+    VCR.use_cassette "No Reviews" do  
+      fill_in "restaurant_name", with: "Custom Restaurant Name"
+      fill_in "restaurant_address", with: "123 Fake Street"
+      fill_in "restaurant_zipcode", with: "12345"
+      fill_in "restaurant_phone_number", with: "3038150200"
+      click_on "Update Info"
+    end
+
+    expect(current_path).to eq(restaurant_path(user.restaurants.first))
+    expect(page).to have_content("Successfully updated restaurant info")
+  end
+end
