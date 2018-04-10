f8cf0b447c5b74aa343d321a3810e8b1d57b4b07
Andrew Dymond <andy.dymond@gmail.com>
2018-04-10 16:40:21 -0600

    Add chef/restaurants_controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   resources :google_users, only: [:create]
   resources :sessions, only: [:create]
   resources :restaurants, only: [:show, :new, :create], param: :slug
+  namespace :chef do
+    resources :restaurants, only: [:edit, :update], param: :slug
+  end
 
   get "/auth/google", as: :google_login
   get 'auth/:provider/callback',  to: 'gsessions#create'
