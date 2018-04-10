1722d57ae21137c6c584e741bf74c7ebb9eb54f7
Andrew Dymond <andy.dymond@gmail.com>
2018-04-10 16:38:00 -0600

    Add chef factory for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -22,4 +22,18 @@ FactoryBot.define do
     end
   end
 
+  factory :chef, class: User do
+    sequence :first_name { |n| "first_name#{n}"}
+    sequence :last_name { |n| "last_name#{n}"}
+    sequence :email { |n| "email#{n}@email.com"}
+    before(:create) do |user|
+      user.create_app_credential(password: "password")
+    end
+    after(:create) do |user|
+      VCR.use_cassette "Factory restaurant" do
+        user.set_restaurant(name: "testaurant1", zipcode: 54321)
+      end
+    end
+  end
+
 end
