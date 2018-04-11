e2f45a53215a2d618f9fd54d5315593c125af417
Andrew Dymond <andy.dymond@gmail.com>
2018-04-10 18:50:54 -0600

    Add stations method to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,15 @@
 class Restaurant < ApplicationRecord
-  has_many :user_roles, dependent: :destroy
-  has_many :roles, through: :user_roles
-  has_many :users, through: :user_roles
   validates_presence_of :name, :zipcode
   validates_uniqueness_of :name, scope: [:zipcode]
   before_save :generate_slug, if: :name_changed?
+  has_many :user_roles, dependent: :destroy
+  has_many :roles, through: :user_roles
+  has_many :users, through: :user_roles
+  has_many :recipes, dependent: :destroy
+
+  def stations
+    recipes.pluck(:station).uniq
+  end
 
   def to_param
     slug
