d44356c4b9dc822b677969d1456fc799d0db8c65
Andrew Dymond <andy.dymond@gmail.com>
2018-04-10 20:05:16 -0600

    Add slug generation & scope name validation to restaurant_id

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,23 @@
 class Recipe < ApplicationRecord
-  validates_uniqueness_of :name
+  validates_presence_of :name
+  validates_uniqueness_of :name, { scope: :restaurant_id }
+  before_save :generate_slug, if: :name_changed?
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   has_many :instructions
   belongs_to :restaurant
 
+  def to_param
+    slug
+  end
+
   def self.filter_by_station(station)
     where(station: station)
   end
+
+  private
+
+    def generate_slug
+      self.slug = name.parameterize
+    end
 end
