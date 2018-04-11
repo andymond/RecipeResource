5af9e0590ac44ccc74ad906ab99d797a8c49a2d6
Andrew Dymond <andy.dymond@gmail.com>
2018-04-10 20:04:10 -0600

    Add cock_can_interact_with_individual_recipes_spec

---
+++ spec/features/cook/cook_can_interact_with_individual_recipes_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+describe "cook visits recipe show" do
+  it "displays recipe instructions, ingredients, and image to cook" do
+    user          = create(:cook)
+    restaurant    = user.restaurants.first
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    recipe        = restaurant.recipes.create(name: "squash ravioli", station: "pasta")
+    squash        = Ingredient.find_or_create_by(name: "squash")
+    ravioli       = Ingredient.find_or_create_by(name: "ravioli")
+    amount_1      = recipe.recipe_ingredients.create(ingredient_id: squash.id, quantity: 1.8, unit: "C")
+    amount_2      = recipe.recipe_ingredients.create(ingredient_id: ravioli.id, quantity: 1.4, unit: "C")
+    instruction_1 = recipe.instructions.create(step: "put the squash in the ravioli")
+    instruction_2 = recipe.instructions.create(step: "boil those lil guys")
+    instruction_3 = recipe.instructions.create(step: "cover with some sauce and enjoy!")
+
+    VCR.use_cassette "No Reviews" do
+      visit dashboard_index_path
+    end
+
+    VCR.use_cassette "No Reviews" do
+      click_on "squash ravioli"
+    end
+
+    expect(current_path).to eq(restaurant_recipe_path(restaurant, recipe))
+    expect(page).to have_content(squash.name)
+    expect(page).to have_content(ravioli.name)
+    expect(page).to have_content(amount_1.quantity)
+    expect(page).to have_content(amount_1.unit)
+    expect(page).to have_content(amount_2.quantity)
+    expect(page).to have_content(amount_2.unit)
+    expect(page).to have_content(instruction_1.step)
+    expect(page).to have_content(instruction_2.step)
+    expect(page).to have_content(instruction_3.step)
+  end
+end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -8,6 +8,18 @@ describe Recipe do
   end
 
   describe "validations" do
-    it {should validate_uniqueness_of(:name)}
+    it {should validate_uniqueness_of(:name).scoped_to(:restaurant_id)}
+  end
+
+  describe "class methods" do
+    it "filters recipes by station" do
+      restaurant = create(:restaurant)
+      recipe_1 = restaurant.recipes.create(name: "soup", station: "garde a mange")
+      recipe_2 = restaurant.recipes.create(name: "salad", station: "garde a mange")
+      recipe_3 = restaurant.recipes.create(name: "pork loin", station: "saute")
+
+      expect(Recipe.filter_by_station("garde a mange")).to eq([recipe_1, recipe_2])
+      expect(Recipe.filter_by_station("saute")).to eq([recipe_3])
+    end
   end
 end
