7562f6b49f54cc9edfa17b84e103ef2abc520f7f
Andrew Dymond <andy.dymond@gmail.com>
2018-04-10 20:07:14 -0600

    Add correct links to recipes in dashboard

---
+++ app/presenters/recipe_presenter.rb
@@ -0,0 +1,30 @@
+class RecipePresenter
+
+  def initialize(slug)
+    @recipe = Recipe.find_by(slug: slug)
+  end
+
+  def ingredients
+    recipe.ingredients.map { |i| i.name }
+  end
+
+  def quantities
+    recipe.recipe_ingredients.map { |ri| ri.quantity}
+  end
+
+  def units
+    recipe.recipe_ingredients.map { |ri| ri.unit}
+  end
+
+  def ingredients_with_quantities_and_units
+    ingredients.zip(quantities, units)
+  end
+
+  def instructions
+    recipe.instructions.map { |i| i.step }
+  end
+
+  private
+    attr_reader :recipe
+
+end
