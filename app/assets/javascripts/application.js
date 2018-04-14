2de1d5a9e941d0cf63f1b80fd4737262571b228f
Andrew Dymond <andy.dymond@gmail.com>
2018-04-13 21:13:31 -0600

    Add javascript for dynamic recipe update form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
 
 // $(document).ready(function() {
@@ -82,9 +84,10 @@ login.addEventListener('click', function() {
 function addIngredient(divName){
   var newdiv = document.createElement('div');
   newdiv.id = "ingSet" + (ingredient_counter + 1)
-  newdiv.innerHTML = "<input type='text' placeholder='ingredient name' name='ingredients[" + (ingredient_counter + 1) + "]' id=ingredient" + (ingredient_counter + 1) + " required>";
-  newdiv.innerHTML += "<input type='number' placeholder='qty' step='0.1' name='qty[" + (ingredient_counter + 1) + "]' class='qty' id=quantity" + (ingredient_counter + 1) + ">";
-  newdiv.innerHTML += "<input type='text' placeholder='unit' name='unit[" + (ingredient_counter + 1) + "]' class='unit' id=unit" + (ingredient_counter + 1) + ">";
+  newdiv.className = "ingredient-row"
+  newdiv.innerHTML = "<input type='text' placeholder='ingredient name' name='ingredients[" + (ingredient_counter + 1) + "]' class='ingredient-data' id=ingredient" + (ingredient_counter + 1) + " required>";
+  newdiv.innerHTML += "<input type='number' placeholder='qty' step='0.1' name='qty[" + (ingredient_counter + 1) + "]' class='qty ingredient-data' id=quantity" + (ingredient_counter + 1) + ">";
+  newdiv.innerHTML += "<input type='text' placeholder='unit' name='unit[" + (ingredient_counter + 1) + "]' class='unit ingredient-data' id=unit" + (ingredient_counter + 1) + ">";
   newdiv.innerHTML += "<button type='button' onClick='removeField(\"ingSet" + (ingredient_counter + 1) + "\");' class='rounded' id=remove-ingredient'" + (ingredient_counter + 1) + ">Remove</button>"
   document.getElementById(divName).appendChild(newdiv);
   ingredient_counter++;
@@ -93,7 +96,7 @@ function addIngredient(divName){
 function addInstruction(divName){
   var newdiv = document.createElement('div');
   newdiv.id = "instSet" + (instruction_counter + 1)
-  newdiv.innerHTML = "<input type='text' placeholder='step " + (instruction_counter + 1) + "' name='instructions[" + (instruction_counter + 1) + "]' class='instruction' id=instruction" + (instruction_counter + 1) + " required>";
+  newdiv.innerHTML = "<input type='text' plac-dataeholder='step " + (instruction_counter + 1) + "' name='instructions[" + (instruction_counter + 1) + "]' class='instruction' id=instruction" + (instruction_counter + 1) + " required>";
   newdiv.innerHTML += "<button type='button' onClick='removeField(\"instSet" + (instruction_counter + 1) + "\");' class='rounded' id='remove-ingredient'" + (instruction_counter + 1) + ">Remove</button>"
   document.getElementById(divName).appendChild(newdiv);
   instruction_counter++;
@@ -104,26 +107,32 @@ function removeField(divName){
   instruction_counter--;
 }
 
-function updateRecipe(){
-  console.log('boo  ')
-  const recipeIngredients = document.querySelector('.recipe-ingredients')
-  const ingredients       = [...recipeIngredients.querySelectorAll('tr')]
-  const recipe = ingredients.map(row => {
-    let rowData = row.querySelectorAll('td')
+function updateRecipe(event){
+  const {recslug, resslug} = {...event.target.dataset}
+  const recipeIngredients  = document.querySelector('.recipe-ingredients')
+  const ingredients        = [...recipeIngredients.querySelectorAll('.ingredient-row')]
+  const recipeInstructions = document.querySelector('.instructions')
+  const instructions       = [...recipeInstructions.querySelectorAll('.instruction')]
+  const ingredientList = ingredients.map(row => {
+    let rowData = row.querySelectorAll('.ingredient-data')
     return {
-      ingredient: rowData[0].innerText,
-      qty: rowData[1].innerText,
-      unit: rowData[2].innerText
+      ingredient: rowData[0].innerText || rowData[0].value,
+      qty: rowData[1].innerText || rowData[1].value,
+      unit: rowData[2].innerText || rowData[2].value
     }
   })
-  putUpdateRecipe(recipe, resSlug, recSlug)
+  debugger
+  const instructionList = instructions.map(row => row.innerText || row.value)
+  putUpdateRecipe({ingredientList, instructionList}, resslug, recslug)
 }
 
-function async putUpdateRecipe(recipe, resSlug, recSlug) {
-  const url = `/restaurants/${resSlug}/recipes/${recSlug}/`
-  const response = await fetch(url, {
-    method: "PUT",
-    headers: {Content-Type: "application/json"},
-    body: JSON.stringify(recipe)
+function putUpdateRecipe(recipe, resSlug, recSlug) {
+  const url = `/chef/restaurants/${resSlug}/recipes/${recSlug}`
+  console.log(url)
+  return jQuery.ajax({
+    type: 'PUT',
+    url: url,
+    contentType: 'application/json',
+    data: JSON.stringify({recipe})
   })
 }
