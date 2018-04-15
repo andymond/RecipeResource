0aeec62609274305cc84306e2af9cac03ae59a31
Andrew Dymond <andy.dymond@gmail.com>
2018-04-15 12:04:22 -0600

    Add guard clause to titleize_name method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name, { scope: :restaurant_id }
   before_save :generate_slug, if: :name_changed?
+  before_save :titleize_name
   before_save :downcase_station
   has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
@@ -24,5 +25,9 @@ class Recipe < ApplicationRecord
 
     def downcase_station
       self.station = station.downcase unless station.nil?
+    end
+
+    def titleize_name
+      self.name = name.titleize unless station.nil?
     end
 end
