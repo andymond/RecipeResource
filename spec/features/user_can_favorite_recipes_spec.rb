5f9d7beb02ab56b9d80faabc4acbb9995acc062f
Andrew Dymond <andy.dymond@gmail.com>
2018-04-16 13:04:00 -0600

    User can view favorites index spec passing

--- spec/features/user_can_favorite_recipes_spec.rb
+++ spec/features/user_can_favorite_recipes_spec.rb
@@ -23,26 +23,31 @@ describe "Any user" do
     expect(user.recipes.empty?).to be(true)
   end
 
-  xit "can view a list of all their favorite recipes" do
-    user   = create(:chef)
+  it "can view a list of all their favorite recipes", js: true do
+    user     = create(:chef)
     recipe_1 = user.restaurants.first.recipes.create(name: "cat food", station: "litter box")
     recipe_2 = user.restaurants.first.recipes.create(name: "dog food", station: "litter box")
     recipe_3 = user.restaurants.first.recipes.create(name: "fish food", station: "litter box")
-    user.favorites(recipe_id: recipe_1.id)
-    user.favorites(recipe_id: recipe_2.id)
-    user.favorites(recipe_id: recipe_3.id)
+    user.favorites.create(recipe_id: recipe_1.id)
+    user.favorites.create(recipe_id: recipe_2.id)
+    user.favorites.create(recipe_id: recipe_3.id)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit user_favorites_path(user)
+    VCR.use_cassette "No Reviews" do
+      visit root_path
+    end
 
+    find(".open-card").click
+
+    VCR.use_cassette "No Reviews" do
+      within ".user-card" do
+        find(".link-to-favorites-index").click
+      end
+    end
+
+    expect(current_path).to eq(user_favorites_path(user))
     expect(page).to have_content(recipe_1.name)
     expect(page).to have_content(recipe_2.name)
     expect(page).to have_content(recipe_3.name)
-
-    within(".dog-food") do
-      find(".favorite").click
-    end
-
-    expect(page).to_not have_content(recipe_2.name)
   end
 end
