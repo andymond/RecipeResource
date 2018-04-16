036ea029d73a319211179f0ff60f0e4b16520774
Andrew Dymond <andy.dymond@gmail.com>
2018-04-16 13:04:00 -0600

    User can view favorites index spec passing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :dashboard, only: [:index]
 
   resources :users, only: [:create] do
-    resources :favorites, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy, :index]
   end
   resources :google_users, only: [:create]
   resources :sessions, only: [:create]
